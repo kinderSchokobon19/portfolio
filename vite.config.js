c82968d237011c5e47cc9f6a9ea07b68ad70bd4e
Verdes Thibault <thibault.verdes@student-cs.fr>
2025-12-05 16:24:57 +0100

    Activation de Tailwind et correction du base path (./)

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+// Importation du plugin Tailwind CSS
 import tailwindcss from '@tailwindcss/vite'; 
 
 const repoName = 'portfolio';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `/${repoName}/`,
+  // CORRECTION 1 : Utilisez le chemin de base relatif './'
+  // C'est la solution la plus fiable pour éviter les 404 sur GitHub Pages.
+  base: './', 
+  
   plugins: [
-    react()
+    react(),
+    // CORRECTION 2 : Activation du plugin Tailwind CSS
+    tailwindcss() 
   ],
 });
