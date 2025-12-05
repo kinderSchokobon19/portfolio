7d681c79971fb50dae1367279ee8b0e18b16a314
Verdes Thibault <thibault.verdes@student-cs.fr>
2025-12-05 16:56:07 +0100

    Correction finale du base path (./) et activation de Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const basename = '/portfolio/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
