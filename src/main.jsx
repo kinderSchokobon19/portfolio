14ec041fbc7c3c2a9c2116dc0f0dade5114aa1b2
Verdes Thibault <thibault.verdes@student-cs.fr>
2025-12-05 15:35:01 +0100

    Mise à jour configuration build

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx';
 import './index.css'; 
 import { BrowserRouter } from 'react-router-dom';
 
+const basename = '/VOTRE_NOUVEAU_NOM_DE_DEPOT/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 // 💡 IMPORTANT : Il faut importer la fonction par défaut (tailwindcss)
 import tailwindcss from '@tailwindcss/vite'; 
 
+const repoName = 'portfolio';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [
     react()
   ],
