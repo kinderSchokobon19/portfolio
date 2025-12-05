50792cbccfdbb9e95975bffb72969883dbfbf967
Verdes Thibault <thibault.verdes@student-cs.fr>
2025-12-05 16:19:39 +0100

    Activation du plugin Tailwind et nouveau build correct

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// 💡 IMPORTANT : Il faut importer la fonction par défaut (tailwindcss)
+
 import tailwindcss from '@tailwindcss/vite'; 
 
 const repoName = 'portfolio';
